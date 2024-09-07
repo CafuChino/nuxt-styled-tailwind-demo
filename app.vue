909a9852e91bd8cbee510a366ebb80e4c87b6db1
CafuChino <kirino@cafuchino.cn>
2024-09-07 14:05:12 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import InputText from 'primevue/inputtext';
+import ToggleButton from 'primevue/togglebutton';
+</script>
+
+<template>
+    <div class="p-5 h-screen bg-gray-900 flex items-center justify-center">
+        <Button label="Click" />
+        <InputText placeholder="Enter your search" />
+        <ToggleButton onLabel="On" offLabel="Off" />
+    </div>
+</template>
